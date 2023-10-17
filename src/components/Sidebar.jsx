72efd64827e9a37b7ddb2dfd27e309c82fcda636
Rajesh Peketi <122203953+Rajesh0078@users.noreply.github.com>
2023-10-18 00:29:54 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,27 +15,27 @@ const Sidebar = () => {
             <div className='mt-5 text-gray-100 '>
                 <NavLink to={'/'} className=" p-2 flex items-center rounded-md mb-1">
                     <HiViewGrid fill='white' size={20} className='inline me-1 ' />
-                    <span className=''>Dashboard</span>
+                    <span className='max-[480px]:hidden'>Dashboard</span>
                 </NavLink>
                 <NavLink to={'/products'} className="p-2 flex items-center rounded-md mb-1">
                     <CgProductHunt fill='white' size={20} className='inline me-1 ' />
-                    <span className=''>Products</span>
+                    <span className='max-[480px]:hidden'>Products</span>
                 </NavLink>
                 <NavLink to={'/customers'} className="p-2 flex items-center rounded-md mb-1">
                     <FaPeopleRoof fill='white' size={20} className='inline me-1 ' />
-                    <span className=''>Customers</span>
+                    <span className='max-[480px]:hidden'>Customers</span>
                 </NavLink>
                 <NavLink to={'/income'} className="p-2 flex items-center rounded-md mb-1">
                     <BiMoneyWithdraw fill='white' size={20} className='inline me-1 ' />
-                    <span className=''>Income</span>
+                    <span className='max-[480px]:hidden'>Income</span>
                 </NavLink>
                 <NavLink to={'/promote'} className="p-2 flex items-center rounded-md mb-1">
                     <FaAward fill='white' size={20} className='inline me-1 ' />
-                    <span className=''>Promote</span>
+                    <span className='max-[480px]:hidden'>Promote</span>
                 </NavLink>
                 <NavLink to={'/help'} className="p-2 flex items-center rounded-md mb-1">
                     <BiHelpCircle fill='white' size={20} className='inline me-1 ' />
-                    <span className=''>Help</span>
+                    <span className='max-[480px]:hidden'>Help</span>
                 </NavLink>
             </div>
         </div>
